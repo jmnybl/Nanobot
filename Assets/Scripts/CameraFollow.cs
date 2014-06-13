054d814cb6700eee5fc310166a849a4c2d9858be
jmnybl <jmnybl@utu.fi>
2014-06-13 23:19:03 +0300

    Zooming camera in/out depending on player's speed.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -7,7 +8,9 @@ public class CameraFollow : MonoBehaviour
     private static GameObject square;
     private static GameObject heavyCircle;
     private static GameObject lightCircle;
-    private float cameraDistance = -10.0f;
+	private float cameraDistance = -10.0f;
+	private float zoom_out = 100.0f;
+	private float zoom_in_max = 50.0f;
 
     void Start()
     {
@@ -19,21 +22,39 @@ public class CameraFollow : MonoBehaviour
     
     void Update()
     {
-        if (square.activeSelf)
-        {
-            transform.position = new Vector3(square.transform.position.x, square.transform.position.y, cameraDistance);
-        }
-        else if (heavyCircle.activeSelf)
-        {
-            transform.position = new Vector3(heavyCircle.transform.position.x, heavyCircle.transform.position.y, cameraDistance);
-        }
-        else if (lightCircle.activeSelf)
-        {
-            transform.position = new Vector3(lightCircle.transform.position.x, lightCircle.transform.position.y, cameraDistance);
-        }
-        else
-        {
-            // do nothing
-        }
+
+
+        if (square.activeSelf) {
+			// this has no rigidbody, so fixed camera zooming
+			transform.position = new Vector3 (square.transform.position.x, square.transform.position.y, cameraDistance);
+			camera.fieldOfView = zoom_out;
+
+		} 
+		else {
+			Vector2 vel = new Vector2(0,0);
+			if (heavyCircle.activeSelf) {
+				transform.position = new Vector3 (heavyCircle.transform.position.x, heavyCircle.transform.position.y, cameraDistance);
+				vel = heavyCircle.rigidbody2D.velocity;
+			}
+			else if (lightCircle.activeSelf) {
+				transform.position = new Vector3 (lightCircle.transform.position.x, lightCircle.transform.position.y, cameraDistance);
+				vel = lightCircle.rigidbody2D.velocity;
+			}
+			else {
+				vel = new Vector2(0,0);
+			}
+			// TODO: smoothing
+			float speed = (Math.Abs (vel.x) + Math.Abs (vel.y)) * 20; // calculate the 'actual' speed and multiply it with 'something' to get suitable zooming
+			if (zoom_in_max > speed) {
+				camera.fieldOfView = zoom_in_max;
+			} 
+			else if (zoom_out < speed) {
+				camera.fieldOfView = zoom_out;
+			} 
+			else {
+				camera.fieldOfView = speed;
+			}
+			
+		}
     } 
 }
