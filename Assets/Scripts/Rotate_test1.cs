72f55c16f9cc0228343211e56e3d0d84872a6e7c
Mig26 <mikko.mjm@gmail.com>
2014-08-02 08:48:47 +0300

    Oil, gears and scripts
    Replaced old red placeholder gears with (static) oil sprite (needs to be
    animated!). Also, it looks like that player will die before it collides
    with oil.
    Added some bg gears, and related to this, I tweaked (copies of) the
    rotation script to make them rotate in a sync (though I'm not using
    exact values there so in time they will be out of sync but hopefully
    no-one will stare at them for hours...)

---
+++ Assets/Scripts/Rotate_test1.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate_test1 : MonoBehaviour
+{
+	private float moveSpeed = 2.4444444f;
+	
+	void Update()
+	{
+		transform.Rotate(Vector3.forward * Time.deltaTime * moveSpeed);
+	}
+}
---
+++ Assets/Scripts/Rotate_test2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate_test2 : MonoBehaviour
+{
+	private float moveSpeed = 1f;
+	
+	void Update()
+	{
+		transform.Rotate(Vector3.back * Time.deltaTime * moveSpeed);
+	}
+}
